927192d2ed0e201ad760bc1509d361b83491bb2a
Qian Dong <qdong327@gmail.com>
2020-10-19 18:44:38 -0400

    added api routes file

---
+++ routes/apiroutes.js
@@ -0,0 +1,18 @@
+let data = require("../db/db.json");
+
+const app = require("express").Router;
+
+module.exports = function(app) {
+    app.get("/api/notes", function(req, res) {
+        console.log(data);
+        return data;
+    });
+
+    app.post("/api/notes", function(req, res) {
+        data.oush(json.stringify(req.body));
+    });
+
+    app.post("/api/notes/:id", function(req, res) {
+        console.log("test" + id);
+    })
+}
