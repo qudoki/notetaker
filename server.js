bab627a5baa3f3133d9a6f6cbda12580f212cb23
Qian Dong <qdong327@gmail.com>
2020-10-19 20:50:46 -0400

    modified server js file

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 3000;
-const apiRoutes = require("./routes/apiroutes");
-const htmlRoutes = require("./routes/htmlroutes");
+//Dependencies
+const express = require("express");
+const fs = require("fs");
 
+//Sets up Express
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+//Express ap to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("assets", express.static("assets"));
 
 require("./routes/apiroutes")(app);
 require("./routes/htmlroutes")(app);
 
+//Server starts listening
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
