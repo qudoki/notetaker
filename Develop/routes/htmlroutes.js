27309ba17e679a39ed1c69d522c9caaa408f64c9
Qian Dong <qdong327@gmail.com>
2020-10-20 23:15:49 -0400

    added in back the html page served at /

--- Develop/routes/htmlroutes.js
+++ Develop/routes/htmlroutes.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const app = require("express").Router();
 module.exports = function (app) {
-    
+
     app.get("/assets/js/index.js", function (req, res) {
         res.sendFile(path.join(__dirname, "/../assets/js/index.js"));
     });
@@ -12,6 +12,10 @@ module.exports = function (app) {
 
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "/../public/notes.html"));
+    });
+
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "/../public/index.html"));
     });
 
     app.get("*", function (req, res) {
